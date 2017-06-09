89c8f7ed31081d03dacc67958401ef2bd69fea01
Violet Cheng <vcheng@pivotal.io>
2017-06-10 07:46:01 +0800

    change enum name

--- shield/pg/pgcommon.go
+++ shield/pg/pgcommon.go
@@ -2,54 +2,74 @@ package pg
 
 // PlanState enum
 const (
-	PlanState   = 200 + iota
-	ResultState //201
-	ModifyTableState
-	AppendState
-	MergeAppendState
-	RecursiveUnionState
-	BitmapAndState
-	BitmapOrState
-	ScanState
-	SeqScanState //209
-	SampleScanState
-	IndexScanState
-	IndexOnlyScanState
-	BitmapIndexScanState
-	BitmapHeapScanState
-	TidScanState
-	SubqueryScanState
-	FunctionScanState
-	ValuesScanState
-	CteScanState
-	WorkTableScanState
-	ForeignScanState
-	CustomScanState
-	JoinState
-	NestLoopState
-	MergeJoinState
-	HashJoinState
-	MaterialState
-	SortState
-	GroupState
-	AggState
-	WindowAggState
-	UniqueState
-	GatherState
-	HashState
-	SetOpState
-	LockRowsState
-	LimitState
+	T_Invalid = iota
+
+	/*
+	 * TAGS FOR EXECUTOR NODES (execnodes.h)
+	 */
+	T_IndexInfo
+	T_ExprContext
+	T_ProjectionInfo
+	T_JunkFilter
+	T_ResultRelInfo
+	T_EState
+	T_TupleTableSlot
+
+	/*
+	 * TAGS FOR PLAN NODES (plannodes.h)
+	 */
+	T_Plan
+	T_Result
+	T_ProjectSet
+	T_ModifyTable
+	T_Append
+	T_MergeAppend
+	T_RecursiveUnion
+	T_BitmapAnd
+	T_BitmapOr
+	T_Scan
+	T_SeqScan
+	T_SampleScan
+	T_IndexScan
+	T_IndexOnlyScan
+	T_BitmapIndexScan
+	T_BitmapHeapScan
+	T_TidScan
+	T_SubqueryScan
+	T_FunctionScan
+	T_ValuesScan
+	T_TableFuncScan
+	T_CteScan
+	T_NamedTuplestoreScan
+	T_WorkTableScan
+	T_ForeignScan
+	T_CustomScan
+	T_Join
+	T_NestLoop
+	T_MergeJoin
+	T_HashJoin
+	T_Material
+	T_Sort
+	T_Group
+	T_Agg
+	T_WindowAgg
+	T_Unique
+	T_Gather
+	T_GatherMerge
+	T_Hash
+	T_SetOp
+	T_LockRows
+	T_Limit
 )
 
 // PlanStateString is the map of enum and print strings
 var planStateStringMap = map[int]string{
-	ResultState:   "Result",
-	SeqScanState:  "Seq Scan",
-	LimitState:    "Limit",
-	AggState:      "Aggregate",
-	NestLoopState: "Nested Loop",
-	MaterialState: "Materialize",
+	T_Result:   "Result",
+	T_SeqScan:  "Seq Scan",
+	T_Limit:    "Limit",
+	T_Agg:      "Aggregate",
+	T_NestLoop: "Nested Loop",
+	T_Material: "Materialize",
 }
 
 func GetNodeTypeString(typeCode int) string {
